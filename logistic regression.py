e7562421f61c66d27b8817d8c873008fb55a559e
sabahomarkm <57164725+sabahomarkm@users.noreply.github.com>
2021-05-05 20:17:47 +0530

    dataset study

--- logistic regression.py
+++ logistic regression.py
@@ -8,22 +8,43 @@ import pandas as pd
 import numpy as np
 
 
-# In[11]:
+# In[23]:
 
 
 pd.set_option('display.max_columns', None)
-churndata=pd.read_csv('G:\DSAICourse\python\Mca Project\dataset\Hz-CTU13_1.csv')#r is used to read as raw data
+churndata=pd.read_csv(r'G:\DSAICourse\python\Mca Project\dataset\Hz-CTU13_1.csv')#r is used to read as raw data
 churndata
 
 
-# In[12]:
+# In[24]:
 
 
 churndata.shape
 
 
-# In[13]:
+# In[25]:
 
 
 churndata.describe()
+
+
+# In[19]:
+
+
+churndata.isnull().sum()
+
+
+# In[22]:
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.figure(figsize=(20,20))
+sns.heatmap(churndata.corr(),annot=True)
+
+
+# In[ ]:
+
+
+
 
